43ef11270f2dd63be988abc18a2e01b9d44dd52e
Ibrahim Saberi <georgeipsum@gmail.com>
2022-12-11 22:11:33 -0500

    add oh-my-zsh installation

--- os/macos/install.sh
+++ os/macos/install.sh
@@ -2,3 +2,6 @@
 
 echo "› sudo softwareupdate -i -a"
 sudo softwareupdate -i -a
+
+# install oh-my-zsh unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
